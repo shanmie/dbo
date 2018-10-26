6d468091e5766483ef907b6ff7e0bc138488d1b6
deacon <3058023150@qq.com>
2018-10-26 21:25:10 +0800

    first commit

---
+++ src/main/java/cn/org/zax/factory/ConnectionFactory.java
@@ -0,0 +1,12 @@
+package cn.org.zax.factory;
+
+/**
+ * @Package: cn.org.zax.factory
+ * @ClassName: ConnectionFactory
+ * @Description:
+ * @Author: mac-pro
+ * @CreateDate: 2018/10/26 下午8:05
+ * @Version: 1.0
+ */
+public class ConnectionFactory {
+}
