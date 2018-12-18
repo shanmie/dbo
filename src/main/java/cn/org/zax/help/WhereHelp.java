f6b18b87d9c97fd0961fa8b55f5b923f5c25aca9
deacon <3058023150@qq.com>
2018-12-18 17:01:04 +0800

    sql where help

--- src/main/java/cn/org/zax/help/WhereHelp.java
+++ src/main/java/cn/org/zax/help/WhereHelp.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class WhereHelp {
     /**
-     *
+     * like or
      * @param sql
      * @param collect 需要like的集合值
      * @param fieldName 需要like的字段名
@@ -28,6 +28,30 @@ public class WhereHelp {
             where += " "+fieldName+" like '%"+collect.get(i)+"%'";
             if (i!= collect.size()-1){
                 where = where +" or";
+            }
+        }
+        where = where + ")";
+        sql += where;
+        return sql;
+    }
+
+    /**
+     * like and
+     * @param sql
+     * @param collect 需要like的集合值
+     * @param fieldName 需要like的字段名
+     * @param joinMark like前的链接符号
+     * @return
+     */
+    private String andLike(String sql, List<Long> collect,String fieldName,String joinMark){
+        String where ="(";
+        if (joinMark !=null){
+            where = joinMark + where;
+        }
+        for (int i = 0; i < collect.size(); i++) {
+            where += " "+fieldName+" like '%"+collect.get(i)+"%'";
+            if (i!= collect.size()-1){
+                where = where +" and";
             }
         }
         where = where + ")";
